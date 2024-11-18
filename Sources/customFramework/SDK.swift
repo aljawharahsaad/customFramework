53acbf6a153a4aede7a781584a0d59f724c8387a
aljawharah <aljawharah@qawam.ai>
2024-11-18 16:52:46 +0300

    first try

---
+++ Sources/customFramework/SDK.swift
@@ -0,0 +1,13 @@
+//
+//  SDK.swift
+//  customFrameworkProject
+//
+//  Created by aljawharah almasoud on 16/05/1446 AH.
+//
+import Foundation
+
+public struct SDK{
+    public static func doSomeWork(){
+        print("doing some work....")
+    }
+}
